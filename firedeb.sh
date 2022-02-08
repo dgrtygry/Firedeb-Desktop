5c5f95c491c6befae5680c3a5d8c937561eb3216
drigy982 <76979689+dgrtygry@users.noreply.github.com>
2022-02-08 07:17:07 -0600

    Add files via upload

---
+++ firedeb.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+kwin &
+/System/Applications/komorebi &
+plank &
+chromium
---
+++ scripts/Install firedeb.sh
@@ -0,0 +1,32 @@
+sudo mv /home/admini/Downloads/firedeb.desktop /usr/share/xsessions
+
+sudo mv /home/admini/Downloads/firedeb.sh /bin
+
+cd /bin
+
+chmod +x firedeb.sh
+
+cd /home/admini/Downloads
+
+sudo apt install figlet
+
+sudo apt install kde-plasma-desktop
+
+sudo apt install chromium
+
+sudo apt install plank
+
+sudo apt install sddm
+
+sudo apt install gdebi
+
+sudo apt install xfce4-appfinder
+
+sudo gdebi /home/admini/Downloads/komorebi-2.1-64-bit.deb
+
+sudo mkdir /home/admini/Desktop/Logout
+
+sudo mv /home/admini/Downloads/scripts/logout.sh /home/admini/Desktop/Logout
+
+sudo figlet Thank you for installing FireDeb or Debian-Web! If you check your display manager you should see "Debian Web" as a desktop option.
+
